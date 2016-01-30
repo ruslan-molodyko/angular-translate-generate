574a6254a4785645ec4b0e6926f5447b11edc29c
ruslanmolodyko <molotn1@ukr.net>
2016-01-30 16:00:15 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rmolodyko on 30.01.2016.
+ */
+
+var fs = require('fs'),
+    Class = require('class.js');
+
+var Parser = Class.create(function() {
+    this.constructor = function() {
+        console.log('hello');
+    };
+});
