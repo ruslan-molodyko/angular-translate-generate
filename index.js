965af3aa7f209218b3f944a1e7af242fc2fa05c3
ruslan-molodyko <molodyko13@gmail.com>
2016-01-31 18:01:03 +0200

    Added endpoint to module

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 31.01.2016.
+ */
+var TransKeys = require('./generate_keys'),
+    TransView = require('./generate_view');
+
+module.exports = {
+   TransKeys: TransKeys,
+   TransView: TransView
+};
